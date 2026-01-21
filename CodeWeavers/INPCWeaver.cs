05db8557c21fbb6417fc0fe0b97717371a2e25b6
俊杰 冯 <Axvser@outlook.com>
2026-01-22 00:25:13 +0800

    添加项目文件。

---
+++ CodeWeavers/INPCWeaver.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PartialCraft.CSharp.CodeWeavers;
+
+public class INPCWeaver : ClassWeaverCore
+{
+    public override bool CanWeave()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override string GetFileName()
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override string[] GenerateBaseInterfaces()
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override string[] GenerateBaseTypes()
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override string GenerateBody(int depth)
+    {
+        throw new NotImplementedException();
+    }
+}
